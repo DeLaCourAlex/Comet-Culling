2319dcddea652f579c77ed75ec1b0e3d17718241
AlexDeLaCour <alexdlcour@gmail.com>
2023-05-02 13:49:24 +0100

    added captains logs

--- Comet Culling/Assets/Scripts/Spaceship/CaptainLogs.cs
+++ Comet Culling/Assets/Scripts/Spaceship/CaptainLogs.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 
 public class CaptainLogs : MonoBehaviour
 {
+    [SerializeField] GameObject[] captainsLog;
+    [SerializeField] GameObject background;
     bool[] isLogAvailable = new bool[7]; //Array of bools that'll determine whether captain log is available or not
-    public bool hasBeenRead; //Bool to check if player has read the log of each day                                   
+    public bool hasBeenRead; //Bool to check if player has read the log of each day
+    public bool logOpen;
     int availableLogs; 
     // Start is called before the first frame update
     void Start()
@@ -24,12 +27,15 @@ public class CaptainLogs : MonoBehaviour
         for(int i = 0; i < 7; ++i) //Go through the array of bools
         {
             if (TimeManager.Day == (i + 1) && hasBeenRead) //If the day number == the index + 1 && it has been read
+            {
                 isLogAvailable[availableLogs] = true; //Turn the counter index bool on
-            //This way if the day == 3 && the player has interacted with screen
-            //If player didn't interact on day 1 but did on day 2,
-            //The available log would be log no. 2, not log no. 3
-            //Prompting player to interact with screen everyday to unlock logs
-            else
+                                                      //This way if the day == 3 && the player has interacted with screen
+                                                      //If player didn't interact on day 1 but did on day 2,
+                                                      //The available log would be log no. 2, not log no. 3
+                                                      //Prompting player to interact with screen everyday to unlock logs
+            }
+
+            else if (i != availableLogs && hasBeenRead)
                 isLogAvailable[i] = false; //Turn the rest off
 
         }
@@ -41,18 +47,23 @@ public class CaptainLogs : MonoBehaviour
 
             hasBeenRead = false; //Immediately toggle hasBeenRead off so it doesn't update this counter more times
         }
-        DisplayLogs(); 
+        DisplayLogs();
+
+        background.SetActive(logOpen);
     }
 
     void DisplayLogs()
     {
         for (int i = 0; i < 7; ++i) //Go through the array of bools
         {
-            if (isLogAvailable[i])
+            if (isLogAvailable[i] && logOpen)
             {
                 Debug.Log("This is day's " + TimeManager.Day + "'s log, corresponds to index " + i + ", is set to true");
+                captainsLog[i].SetActive(true);
             } 
 
+            else
+                captainsLog[i].SetActive(false);
         }
     }
 }
